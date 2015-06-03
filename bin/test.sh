83603011cbe67a6d414ee74462ef9124d457a4dd
kirkhess <kirk.hess@gmail.com>
2015-06-03 11:15:00 -0400

    test changes; commit: 191cb17

--- bin/test.sh
+++ bin/test.sh
@@ -3,7 +3,7 @@ set -o pipefail
 
 zorba -i -f -q xbin/zorba.xqy -e marcxmluri:="http://lccn.loc.gov/61008194/marcxml" -e serialization:="rdfxml" -e baseuri:="http://bibframe.org/" >/dev/null
 if [ $? == 0 ]; then 
-   echo "Aorba success" 
+   echo "Zorba success" 
 fi
 
 zorba -i -f -q xbin/zorba3-0.xqy -e marcxmluri:="http://lccn.loc.gov/61008194/marcxml" -e serialization:="rdfxml" -e baseuri:="http://bibframe.org/" >/dev/null
@@ -15,6 +15,6 @@ fi
 java -cp ~/.m2/repository/net/sf/saxon/Saxon-HE/9.5.1-8/Saxon-HE-9.5.1-8.jar net.sf.saxon.Query xbin/saxon.xqy marcxmluri="http://lccn.loc.gov/61008194/marcxml" >/dev/null
 
 if [ $? == 0 ]; then
-   echo "Szxon success"
+   echo "Saxon success"
 fi
 
