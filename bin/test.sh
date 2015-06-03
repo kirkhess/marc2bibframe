c0e0b1d04fa11791bbf8e73cd8c4ca4fbf2c6f44
kirkhess <kirk.hess@gmail.com>
2015-06-03 11:29:39 -0400

    changing test url to http://lccn.loc.gov/2011276039/marcxml

--- bin/test.sh
+++ bin/test.sh
@@ -1,18 +1,18 @@
 set -e
 set -o pipefail
 
-zorba -i -f -q xbin/zorba.xqy -e marcxmluri:="http://lccn.loc.gov/61008194/marcxml" -e serialization:="rdfxml" -e baseuri:="http://bibframe.org/" >/dev/null
+zorba -i -f -q xbin/zorba.xqy -e marcxmluri:="http://lccn.loc.gov/2011276039/marcxml" -e serialization:="rdfxml" -e baseuri:="http://bibframe.org/" >/dev/null
 if [ $? == 0 ]; then 
    echo "Zorba success" 
 fi
 
-zorba -i -f -q xbin/zorba3-0.xqy -e marcxmluri:="http://lccn.loc.gov/61008194/marcxml" -e serialization:="rdfxml" -e baseuri:="http://bibframe.org/" >/dev/null
+zorba -i -f -q xbin/zorba3-0.xqy -e marcxmluri:="http://lccn.loc.gov/2011276039/marcxml" -e serialization:="rdfxml" -e baseuri:="http://bibframe.org/" >/dev/null
 
 if [ $? == 0 ]; then
    echo "Zorba3 success"
 fi
 
-java -cp ~/.m2/repository/net/sf/saxon/Saxon-HE/9.5.1-8/Saxon-HE-9.5.1-8.jar net.sf.saxon.Query xbin/saxon.xqy marcxmluri="http://lccn.loc.gov/61008194/marcxml" >/dev/null
+java -cp ~/.m2/repository/net/sf/saxon/Saxon-HE/9.5.1-8/Saxon-HE-9.5.1-8.jar net.sf.saxon.Query xbin/saxon.xqy marcxmluri="http://lccn.loc.gov/2011276039/marcxml" >/dev/null
 
 if [ $? == 0 ]; then
    echo "Saxon success"
